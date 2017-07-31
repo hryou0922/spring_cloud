7276fb764aeaa884645054f88501e843df69a6da
hryou0922 <hryou0922@126.com>
2017-07-31 17:51:00 +0800

    spring cloud config

--- cloud_service/src/main/java/com/hry/spring/cloud/service/simple/SimpleCloudApplication.java
+++ cloud_service/src/main/java/com/hry/spring/cloud/service/simple/SimpleCloudApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleCloudApplication {
 
 	public static void main(String[] args) {
+		// 如果执行成功，会有如下打印信息： Located property source: CompositePropertySource [name='configService', propertySources=[MapPropertySource [name='https://github.com/hryou0922/spring_cloud.git/cloudconfig/cloud-config-dev.properties']]]
 		SpringApplication.run(SimpleCloudApplication.class, args);		
 	}
 }
--- cloud_service/src/main/java/com/hry/spring/cloud/service/simple/SimpleConfig.java
+++ cloud_service/src/main/java/com/hry/spring/cloud/service/simple/SimpleConfig.java
@@ -1,9 +1,7 @@
 package com.hry.spring.cloud.service.simple;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * 从配置服务获取的配置存储到此文件中
@@ -12,12 +10,8 @@ import org.springframework.context.annotation.PropertySource;
  *
  */
 @Configuration
-// @PropertySource(value="classpath:/cloud-config-${spring.profiles.active}.properties")
-// @PropertySource(value="cloud-config-${spring.profiles.active}.properties")
 @ConfigurationProperties(prefix = "simple.config" ,ignoreUnknownFields = false)
 public class SimpleConfig {
-	
-	@Value("${simple.config.name}")
 	private String name;
 	private Integer age;
 	
