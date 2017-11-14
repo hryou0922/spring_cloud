8c9ba43d3e945da663d5eef33eab8bfbc89e37be
hryou0922 <hryou0922@126.com>
2017-11-14 16:33:38 +0800

    spring-boot-starter-parent版本从1.5.2.RELEASE升级到1.5.6.RELEASE; spring-cloud-dependencies版本从Dalston.SR2升级到Dalston.SR4

---
+++ cloud-consumer-hystrix/src/main/java/com/hry/spring/cloud/consumer/hystrix/raw/HystrixSimpleCloudConsumerApplication.java
@@ -0,0 +1,53 @@
+package com.hry.spring.cloud.consumer.hystrix.raw;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableEurekaClient // 配置本应用将使用服务注册和服务发现
+public class HystrixSimpleCloudConsumerApplication {
+
+    public static void main(String[] args) {
+        args = new String[1];
+        args[0] = "--spring.profiles.active=hystrix-simple";
+        SpringApplication.run(HystrixSimpleCloudConsumerApplication.class, args);
+    }
+
+    /**
+     * 初始RestTemplate
+     * @return
+     */
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    /**
+     * 使用fastjson做为json的解析器
+     * @return
+     */
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
+    }
+}
