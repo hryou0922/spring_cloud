4219c82922c2751324d610d7646c339989bfff17
hryou0922 <hryou0922@126.com>
2017-12-20 18:26:09 +0800

    uml

--- cloud-consumer-zipkin/src/main/java/com/hry/spring/cloud/consumer/zipkin/control/SimpleCtl.java
+++ cloud-consumer-zipkin/src/main/java/com/hry/spring/cloud/consumer/zipkin/control/SimpleCtl.java
@@ -2,18 +2,21 @@ package com.hry.spring.cloud.consumer.zipkin.control;
 
 
 import com.hry.spring.cloud.consumer.zipkin.feign.IRibbonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleCtl {
-
+    private static final Logger log = LoggerFactory.getLogger(SimpleCtl.class);
     @Autowired
     private IRibbonClient ribbonClient;
 
 	@RequestMapping(value="/zipkin/simple")
     public String simpleClientCall(){
+        log.info("call " + this.getClass().getSimpleName() + " simpleClientCall" );
 		return "rsp: " + ribbonClient.ribbonClientCall();
     }
 
